df8e88702e8bcdc1e4460696681e386ee2293361
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-04 15:28:00 -0400

    add items to array

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react';
 import Form from './Form';
+import { nanoid } from 'nanoid';
 
 const App = () => {
   const [items, setItems] = useState([]);
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    };
+    setItems([...items, newItem]);
+  };
   return (
     <section className="section-center">
-      <Form />
+      <Form addItem={addItem} />
     </section>
   );
 };
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 
-const Form = () => {
+const Form = ({ addItem }) => {
   const [newItemName, setNewItemName] = useState('');
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(newItemName);
+    if (!newItemName) return;
+    addItem(newItemName);
+    setNewItemName('');
   };
   return (
     <form onSubmit={handleSubmit}>
