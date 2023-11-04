b0102d6faad4280a30961891dae617d697934066
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-04 15:46:37 -0400

    add single item render

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,4 +1,19 @@
-const SingleItem = () => {
-  return <h2>SingleItem</h2>;
+import { useState } from 'react';
+
+const SingleItem = ({ item, removeItem }) => {
+  const [isChecked, setIsChecked] = useState(false);
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <p style={{ textDecoration: isChecked && 'line-through' }}>{item.name}</p>
+      <button type="button" className="btn remove-btn">
+        delete
+      </button>
+    </div>
+  );
 };
 export default SingleItem;
